2b6c246fd4e0dc7951c779e58441fcdbe905ead6
panakuma <c01171055a@edu.teu.ac.jp>
2019-06-02 03:16:17 +0900

    とりあえず完成

--- document_root/api.php
+++ document_root/api.php
@@ -1,7 +1,7 @@
 <?php
   try{
     # GETリクエストからMACアドレスをもらう
-    $_GET["MAC"] = $MAC;
+    $mac = $_GET["mac"];
 
     # OUIデータベースに接続(SQLite)
     $dbcon = new PDO("sqlite:../oui.db");
@@ -10,21 +10,20 @@
     }
 
     # $MACにアドレスが入っていれば、データベースから検索
-    $MAC = mb_strlen(preg_replace('/[^0-9a-fA-F]/', '', $MAC));
-    if($MAC >= 6){
-      $MAC = substr($MAC, 0, 6);
+    $mac = strtolower(preg_replace('/[^0-9a-fA-F]/', '', $mac));
+    if(mb_strlen($mac) >= 6){
+      $mac = substr($mac, 0, 6);
       $stmt = $dbcon -> prepare("select * from `oui` where `mac` like ?;");
-      $stmt -> execute([$MAC]);
+      $stmt -> execute([$mac]);
       $result = $stmt->fetchAll();
-      $data[] = array(
-        'Vender code' => $MAC,
-        'Vender name' => $result['vender']
+      $data = array(
+        'Vender code' => substr($mac, 0, 2) . "-" . substr($mac, 2, 2) . "-" . substr($mac, 4, 2),
+        'Vender name' => substr($result[0]['vender'], 0, -2)
       );
+      header('Access-Control-Allow-Origin: *');
       header('Content-type: application/json');
       echo json_encode($data, JSON_UNESCAPED_UNICODE);
     }
-
-
   }catch (Exception $e) {
     echo $e->getMessage() . PHP_EOL;
   }
